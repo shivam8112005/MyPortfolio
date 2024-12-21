6d2d4962a384a50fb811d83a5b91677730221725
shivam shukla <shivamshukla8112005@gmail.com>
2024-12-21 16:39:42 +0530

    completed navbar componenet and started with three js computer.jsx component model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import {motion} from 'framer-motion'
+import {styles} from '../style'
+import {ComputersCanvas} from './canvas'
+import { section } from 'framer-motion/client'
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+          <div className='w-1 sm:h-80 h-40 violet-g'/>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Shivam</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I specialize in solving coding problems<br className='sm:block hidden'/>, developing dynamic web applications<br className='sm:block hidden'/>, and learning data analysis to uncover insights</p>
+        </div>
+      </div>
+        <ComputersCanvas/>
+    </section>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
+import React,{ Suspense, useEffect, useState} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { meshBounds, OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader'
 const Computers = () => {
+  const computer=useGLTF('./desktop_pc/scene.gltf')
   return (
-    <div>Computers</div>
+  <mesh>
+    <hemisphereLight intensity={0.15} groundColor="black" />
+  </mesh>
   )
 }
 
